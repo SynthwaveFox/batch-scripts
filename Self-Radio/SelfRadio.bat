1f089a1f7fbcd2b05b681a3f6370e4e4a9f8b009
SynthwaveFox <soudert1@hotmail.com>
2026-02-05 17:37:49 -0500

    added readme

---
+++ Self-Radio/SelfRadio.bat
@@ -0,0 +1,92 @@
+@echo off
+echo ==== Phoenix's Self-Radio Downloader Script ====
+title Phoenix's Self-Radio Downloader
+setlocal EnableExtensions EnableDelayedExpansion
+
+set "SELF_RADIO_DIR=%userprofile%\Documents\Rockstar Games\GTAV Enhanced\User Music"
+echo Initializing...
+where yt-dlp.exe >nul 2>nul
+if %errorlevel% equ 0 (
+    echo yt-dlp is installed.
+    echo Updating yt-dlp...
+    yt-dlp -U
+) else (
+    echo yt-dlp is not installed.
+    pause
+)
+
+echo Checking for ffmpeg...
+where ffmpeg.exe >nul 2>nul
+if %errorlevel% equ 0 (
+    echo ffmpeg is installed.
+) else (
+    echo ffmpeg is not installed.
+    pause
+)
+echo Initialization complete.
+GOTO :MainMenu
+
+:: Menu
+:MainMenu
+cls
+echo ==== Main Menu ===
+echo 1. Download Self-Radio Music from YouTube
+echo 2. List Installed Songs
+echo 3. Launch GTA V Enhanced
+echo 4. Exit
+
+    CHOICE /C 1234 /N /M "Please enter your choice [1,2,3,4]:"
+    
+    REM --- Process the choice. Remember to check ERRORLEVEL from HIGHEST to LOWEST ---
+    IF ERRORLEVEL 4 GOTO :ExitScript
+    IF ERRORLEVEL 3 GOTO :Option3
+    IF ERRORLEVEL 2 GOTO :Option2
+    IF ERRORLEVEL 1 GOTO :Option1
+
+:Option1
+    cls
+    echo ==== Download Self-Radio Music ====
+    echo Please enter the YouTube video or playlist URL for Self-Radio:
+    set /p url=URL: 
+    echo Downloading and converting to MP3...
+    yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 -o "Self-Radio\%%(title)s.%%(ext)s" %url%
+    for %%F in ("Self-Radio\*.mp3") do (
+        echo.
+        echo ====
+        echo File: %%F
+
+        set /p ARTIST=Artist:
+        set /p TITLE=Title:
+
+        ffmpeg -y -i "%%F" -map 0 -c copy -metadata artist="!ARTIST!" -metadata title="!TITLE!" "%%~nF.tmp.mp3" -filter:a loudnorm=I=-16:TP=-1.0:LRA=11
+
+        if exist "%%~nF.tmp.mp3" (
+            move /Y "%%~nF.tmp.mp3" "%%F" >nul
+        )
+    )
+    echo Download complete!
+    echo Moving files...
+    move /Y "Self-Radio\*.mp3" "%SELF_RADIO_DIR%" 
+    PAUSE
+    GOTO :MainMenu
+
+:Option2
+    cls
+    echo ==== Installed Songs ====
+    if not exist "%SELF_RADIO_DIR%" (
+        echo No songs installed yet.
+    ) else (
+        dir /b "%SELF_RADIO_DIR%"
+    )
+    PAUSE
+    GOTO :MainMenu
+
+:Option3
+    cls
+    start "" steam://rungameid/3240220
+    exit
+
+:ExitScript
+    ECHO.
+    ECHO Exiting the script.
+    EXIT /B
---
+++ readme.md
@@ -0,0 +1,4 @@
+# User Music Downloader for GTA V/Enhanced
+This script requires yt-dlp and ffmpeg either in the same directory as the .bat or in the PATH system variables.
+
+Downloads, tags, and volume normalizes music from any source yt-dlp can download from.
